4294198991dd7571a9e86b7eaa9e8cfe1c99da42
Drashtant Nayak <drashtantnayak@Drashtants-MacBook-Pro.local>
2022-12-04 15:21:20 -0700

    data fetching

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,42 @@
     <div>This is the main page</div>
     <Counter/>
     <div>
-      x: {{x}}
-      y: {{y}}
+      x: {{ x }}
+      y: {{ y }}
+    </div>
+    <div>
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          {{ user.name}}
+        </li>
+      </ul>
+    </div>
+    <div>
+      <ul>
+        <li v-for="user in users2" :key="user.id">
+          {{ user.name}}
+        </li>
+      </ul>
+    </div>
+    <div>
+      {{user.name}}
     </div>
   </div>
 </template>
 <script setup>
-const {x,y} = useMouse()
+const {x, y} = useMouse()
+
+const users = ref([])
+
+onMounted(() => {
+  fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => users.value = data)
+})
+
+//const { data:users2 } = await useAsyncData('users2',() => $fetch('https://jsonplaceholder.typicode.com/users'))
+// OR
+const {data:users2} = await useFetch('https://jsonplaceholder.typicode.com/users')
+
+const {data:user} = await useFetch('https://jsonplaceholder.typicode.com/users/1')
 </script>
